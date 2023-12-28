67795a2beb647276d30bdf3da771584e6465465f
Richard Goulter <richard.goulter@gmail.com>
2023-12-28 09:52:22 +0700

    qmk porting: keymap: add rgoulter keymap to wabble60

---
+++ qmk_porting/keyboards/wabble60/keymaps/rgoulter/keymap.c
@@ -0,0 +1,1 @@
+#include "layouts/community/ortho_5x12/rgoulter/keymap.c"
---
+++ qmk_porting/keyboards/wabble60/keymaps/rgoulter/qmk_config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "users/rgoulter/config.h"
+#include "layouts/community/ortho_5x12/rgoulter/config.h"
